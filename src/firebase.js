bd096db08c02aaf57fb760351362cef450f4cfde
Liza Shrestha <shresthaliza4@gmail.com>
2026-07-04 23:02:16 +0545

    created env for github security

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAijlrVJzcaLy2LMNS8mI2rBl3SbYMbft8",
-  authDomain: "aideea.firebaseapp.com",
-  projectId: "aideea",
-  storageBucket: "aideea.firebasestorage.app",
-  messagingSenderId: "577131166347",
-  appId: "1:577131166347:web:925cd8da597a537ca1b28e",
-  measurementId: "G-7E6QHDF2H2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
